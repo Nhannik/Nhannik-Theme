7edf0debb01031a0f366d95a6d7941747fff4727
Yahya <yahyaaoo200@gamil.com>
2024-01-31 18:20:29 -0800

    Remove the old colors and Add the new colors with the tokens of them

--- vars.ts
+++ vars.ts
@@ -23,86 +23,156 @@ const toRgba : (hex:string,alpha?:number)=>string  = (hex,alpha=1) => {
   
       // neutral colors
       neutral:{
-        50:"#F2F2F2",
-        100:"#E6E6E6",
-        200:"#D9D9D9",
-        300:"#CCCCCC",
-        400:"#BFBFBF",
-        500:"#999999",
-        600:"#808080",
-        700:"#666666",
-        800:"#4D4D4D",
-        900:"#262626"
+        50:"#F4F4F4",
+        100:"#E0E0E0",
+        200:"#C6C6C6",
+        300:"#A8A8A8",
+        400:"#D8D8D8",
+        500:"#6F6F6F",
+        600:"#525252",
+        700:"#393939",
+        800:"#262626",
+        900:"#161616"
       },
   
   
       // green colors
       green:{
-        50:"#ECF8EF",
-        100:"#C6EBD0",
-        200:"#A0DEB2",
-        300:"#7AD193",
-        400:"#55C474",
-        500:"#42BE65",
+        50:"#DEFBE6",
+        100:"#A7F0BA",
+        200:"#6FDC8C",
+        300:"#42BE65",
+        400:"#24A148",
+        500:"#198038",
         600:"#0E6027",
-        700:"#0A471D",
-        800:"#062E13",
-        900:"#031408"
+        700:"#044317",
+        800:"#022D0D",
+        900:"#071908"
       },
   
       // red colors
       red:{
-        50:"#FBE8E9",
-        100:"#F4BBBE",
-        200:"#EC8E93",
-        300:"#E56168",
-        400:"#DE343D",
+        50:"#FFF1F1",
+        100:"#FFD7D9",
+        200:"#FFB3B8",
+        300:"#FF8389",
+        400:"#FA4D56",
         500:"#DA1E28",
-        600:"#99151C",
-        700:"#6D0F14",
-        800:"#42090C",
-        900:"#160304"
+        600:"#A2191F",
+        700:"#750E13",
+        800:"#520408",
+        900:"#2D0709"
       },
   
       // yellow colors
       yellow:{
-        50:"#FEFCE8",
-        100:"#FDEDD3",
-        200:"#FAE496",
-        300:"#FAD658",
-        400:"#FAC91C",
-        500:"#F5A524",
-        600:"#C4841D",
-        700:"#936316",
-        800:"#62420E",
-        900:"#312107"
+        50:"#FFF7D6",
+        100:"#F8E6A0",
+        200:"#F5CD47",
+        300:"#E2B203",
+        400:"#CF9F02",
+        500:"#B38600",
+        600:"#946F00",
+        700:"#7F5F01",
+        800:"#533F04",
+        900:"#332E1B"
       },
   
       // orange colors
       orange:{
-        50:"#FFDABF",
-        100:"#FFC195",
-        200:"#FFA86A",
-        300:"#FAD658",
-        400:"#FF8F40",
-        500:"#FF832B",
-        600:"#B35C1E",
-        700:"#804216",
-        800:"#4D280D",
-        900:"#1A0D04"
+        50:"#FFF2E8",
+        100:"#FFD9BE",
+        200:"#FFB784",
+        300:"#FF832B",
+        400:"#EB6200",
+        500:"#BA4E00",
+        600:"#8A3800",
+        700:"#5E2900",
+        800:"#3E1A00",
+        900:"#231000"
+      },
+
+      // teal colors
+      teal:{
+        50:"#D9FBFB",
+        100:"#9EF0F0",
+        200:"#3DDBD9",
+        300:"#08BDBA",
+        400:"#009D9A",
+        500:"#007D79",
+        600:"#005D5D",
+        700:"#004144",
+        800:"#022B30",
+        900:"#081A1C"
+      },
+
+      // lime colors
+      lime:{
+        50:"#EFFFD6",
+        100:"#D3F1A7",
+        200:"#B3DF72",
+        300:"#08BDBA",
+        400:"#82B536",
+        500:"#6A9A23",
+        600:"#5B7F24",
+        700:"#4C6B1F",
+        800:"#37471F",
+        900:"#28311B"
+      },
+
+      // cyan colors
+      cyan:{
+        50:"#E7F9FF",
+        100:"#C6EDFB",
+        200:"#9DD9EE",
+        300:"#6CC3E0",
+        400:"#42B2D7",
+        500:"#2898BD",
+        600:"#227D9B",
+        700:"#206A83",
+        800:"#164555",
+        900:"#1E3137"
+      },
+
+      // purple colors
+      purple:{
+        50:"#F3F0FF",
+        100:"#DFD8FD",
+        200:"#B8ACF6",
+        300:"#9F8FEF",
+        400:"#8F7FF7",
+        500:"#8270DB",
+        600:"#6E5DC6",
+        700:"#5E4DB2",
+        800:"#352C63",
+        900:"#2B273F"
+      },
+
+      // magenta colors
+      magenta:{
+        50:"#FFECF8",
+        100:"#FDD0EC",
+        200:"#F797D2",
+        300:"#E774BB",
+        400:"#DA62AC",
+        500:"#CD519D",
+        600:"#AE4787",
+        700:"#943D73",
+        800:"#50253F",
+        900:"#3D2232"
       },
   
       // blue colors
       blue:{
-        50:"#E9F4FF",
-        100:"#BDDEFF",
-        200:"#91C8FF",
-        300:"#65B2FF",
-        400:"#399CFF",
+        50:"#E9F2FF",
+        100:"#CCE0FF",
+        200:"#85B8FF",
+        300:"#579DFF",
+        400:"#388BFF",
         500:"#006FEE",
-        600:"#006FEE",
-        700:"#005BC4",
-        800:"#002E62",
+        600:"#005AD9",
+        700:"#0050FB",
+        800:"#09327C",
         900:"#001731"
       },
   }
@@ -130,12 +200,12 @@ const toRgba : (hex:string,alpha?:number)=>string  = (hex,alpha=1) => {
       // ## icon colors ##
       icon:{
         dark:"#262626",
-        light:mainColor.blue[50],
+        light:mainColor.white,
         blue:mainColor.blue[500],
         secondary:mainColor.neutral[600],
         success:mainColor.green[400],
-        error:mainColor.red[500],
         warning:mainColor.orange[500],
+        error:mainColor.red[500],
         disabled:mainColor.neutral[400],
       },
       // ## border colors ##
@@ -143,19 +213,14 @@ const toRgba : (hex:string,alpha?:number)=>string  = (hex,alpha=1) => {
         focus :mainColor.blue[600] ,
         selected:mainColor.blue[500],
         brand:mainColor.blue[500],
-        dark:mainColor.blue[900],
-        "brand-darker":mainColor.blue[800],
+        "brand-dark":mainColor.blue[900],
+        subtle:mainColor.neutral[200],
+        strong:mainColor.neutral[400],
         error:mainColor.red[400],
         warning:mainColor.orange[500],
         success:mainColor.green[400],
-        subtle:mainColor.neutral[200],
-        strong:mainColor.neutral[400],
-        disabled:mainColor.neutral[100],
         light:mainColor.white,
-        "notification-information":mainColor.blue[100],
-        "notification-error":mainColor.red[200],
-        "notification-warning":mainColor.yellow[400],
-        'notification-success':mainColor.green[500]
+        disabled:mainColor.neutral[100],
       },
       // ## background colors ##
       background:{
@@ -165,7 +230,7 @@ const toRgba : (hex:string,alpha?:number)=>string  = (hex,alpha=1) => {
         "layer2-hover":mainColor.neutral[100],
         layer3:mainColor.neutral[200],
         "layer3-hover":mainColor.neutral[300],
-        selected:mainColor.neutral[400],
+        "layer3-selected":mainColor.neutral[400],
         brand:mainColor.blue[500],
         "brand-dark":mainColor.blue[900],
         "brand-light":mainColor.blue[100],
@@ -185,25 +250,25 @@ const toRgba : (hex:string,alpha?:number)=>string  = (hex,alpha=1) => {
         error:mainColor.red[500],
         warning:mainColor.orange[500],
         success:mainColor.green[500],
-        disabled:mainColor.neutral[300]
+        disabled:mainColor.neutral[400]
       },
       // ## button colors ##
       button:{
         primary:mainColor.blue[500],
         "primary-hover":mainColor.blue[700],
-        "primary-Selected":mainColor.blue[800],
+        "primary-selected":mainColor.blue[800],
   
         secondary:mainColor.neutral[600],
         "secondary-hover":mainColor.neutral[700],
-        "secondary-Selected":mainColor.neutral[800],
+        "secondary-selected":mainColor.neutral[800],
   
-        tertiary:'transparent',
+        tertiary:mainColor.white,
         "tertiary-hover":mainColor.neutral[50],
         "tertiary-Selected":mainColor.neutral[100],
   
         danger:mainColor.red[500],
         "danger-hover":mainColor.red[600],
-        "danger-Selected":mainColor.red[700],
+        "danger-selected":mainColor.red[700],
   
         disabled:mainColor.neutral[100],
       },
@@ -252,68 +317,139 @@ const toRgba : (hex:string,alpha?:number)=>string  = (hex,alpha=1) => {
       // blue purple cool-gray magenta gray warm-gray orange red cyan green teal dark-gray
       tag:{
         blue:{
-          bg:"#D0E2FF",
-          stroke:"#B2D0FF",  
-          text:"#05122D"
+          bg:mainColor.blue[100],
+          hover:mainColor.blue[200],
+          text:mainColor.blue[800],
+          stroke:mainColor.blue[200]
+        },
+        "dark-blue":{
+          bg:mainColor.blue[300],
+          hover:mainColor.blue[400],
+          text:mainColor.blue[900],
+          stroke:mainColor.blue[400]
         },
         purple:{
-          bg:"#E8DAFF",
-          stroke:"#CFB2FF",  
-          text:"#0B0218"
+          bg:mainColor.purple[100],
+          hover:mainColor.purple[200],
+          text:mainColor.purple[800],
+          stroke:mainColor.purple[200]
         },
-        "cool-gray":{
-          bg:"#CDD3DA",
-          stroke:"#ADBCCC",  
-          text:"#0B0E10"
+        "dark-purple":{
+          bg:mainColor.purple[300],
+          hover:mainColor.purple[400],
+          text:mainColor.purple[900],
+          stroke:mainColor.purple[400]
         },
-        magenta:{
-          bg:"#FFD6E8",
-          stroke:"#FFB2D4",  
-          text:"#2D0416"
+        yellow:{
+          bg:mainColor.yellow[100],
+          hover:mainColor.yellow[200],
+          text:mainColor.yellow[800],
+          stroke:mainColor.yellow[200]
         },
-        gray:{
-          bg:"#E0E0E0",
-          stroke:"#C7C7C7",  
-          text:"#161616"
-        },
-        "warm-gray":{
-          bg:"#F1E9E7",
-          stroke:"#CCC4C2",  
-          text:"#0B052D"
-        },
-        orange:{
-          bg:"#FFE0C3",
-          stroke:"#FFC691",  
-          text:"#2B1706"
-        },
-        red:{
-          bg:"#FFD7D9",
-          stroke:"#FFBFC2",  
-          text:"#2B0607"
+        "dark-yellow":{
+          bg:mainColor.yellow[200],
+          hover:mainColor.yellow[300],
+          text:mainColor.yellow[900],
+          stroke:mainColor.yellow[300]
         },
         cyan:{
-          bg:"#BAE6FF",
-          stroke:"#99DAFF",  
-          text:"#041D33"
+          bg:mainColor.cyan[100],
+          hover:mainColor.cyan[200],
+          text:mainColor.cyan[800],
+          stroke:mainColor.cyan[200]
+        },
+        "dark-cyan":{
+          bg:mainColor.cyan[300],
+          hover:mainColor.cyan[400],
+          text:mainColor.cyan[900],
+          stroke:mainColor.cyan[400]
+        },
+        red:{
+          bg:mainColor.red[100],
+          hover:mainColor.red[200],
+          text:mainColor.red[700],
+          stroke:mainColor.red[200]
+        },
+        "dark-red":{
+          bg:mainColor.red[300],
+          hover:mainColor.red[400],
+          text:mainColor.red[900],
+          stroke:mainColor.red[400]
+        },
+        lime:{
+          bg:mainColor.lime[100],
+          hover:mainColor.lime[200],
+          text:mainColor.lime[800],
+          stroke:mainColor.lime[200]
+        },
+        "dark-lime":{
+          bg:mainColor.lime[300],
+          hover:mainColor.lime[400],
+          text:mainColor.lime[900],
+          stroke:mainColor.lime[400]
         },
         green:{
-          bg:"#A7F0BA",
-          stroke:"#8AE5A2",  
-          text:"#021D0A"
+          bg:mainColor.green[100],
+          hover:mainColor.green[200],
+          text:mainColor.green[700],
+          stroke:mainColor.green[200]
+        },
+        "dark-green":{
+          bg:mainColor.green[300],
+          hover:mainColor.green[400],
+          text:mainColor.green[900],
+          stroke:mainColor.green[400]
         },
         teal:{
-          bg:"#9EF0F0",
-          stroke:"#81E5E5",  
-          text:"#052F2F"
+          bg:mainColor.teal[100],
+          hover:mainColor.teal[200],
+          text:mainColor.teal[700],
+          stroke:mainColor.teal[200]
+        },
+        "dark-teal":{
+          bg:mainColor.teal[300],
+          hover:mainColor.teal[400],
+          text:mainColor.teal[900],
+          stroke:mainColor.teal[400]
+        },
+        orange:{
+          bg:mainColor.orange[100],
+          hover:mainColor.orange[200],
+          text:mainColor.orange[700],
+          stroke:mainColor.orange[200]
+        },
+        "dark-orange":{
+          bg:mainColor.orange[300],
+          hover:mainColor.orange[400],
+          text:mainColor.orange[900],
+          stroke:mainColor.orange[400]
+        },
+        magenta:{
+          bg:mainColor.magenta[100],
+          hover:mainColor.magenta[200],
+          text:mainColor.magenta[800],
+          stroke:mainColor.magenta[200]
+        },
+        "dark-magenta":{
+          bg:mainColor.magenta[300],
+          hover:mainColor.magenta[400],
+          text:mainColor.magenta[900],
+          stroke:mainColor.magenta[400]
+        },
+        gray:{
+          bg:mainColor.neutral[100],
+          hover:mainColor.neutral[200],
+          text:mainColor.neutral[900],
+          stroke:mainColor.neutral[200]
         },
         "dark-gray":{
-          bg:"#393939",
-          stroke:"#0D0D0D",  
-          text:"#FFFFFF"
+          bg:mainColor.neutral[700],
+          hover:mainColor.neutral[600],
+          text:mainColor.neutral[50],
+          stroke:mainColor.neutral[900]
         },
       }
   }
-  
   
   
   // screens
