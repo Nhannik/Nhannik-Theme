aa80bc25704c5156e7566b8546dc7cabbb11c16b
MESSELMII_YAHYA <107892703+MESSELMIyahya@users.noreply.github.com>
2024-02-12 22:11:20 +0100

    Finish the Breadcrumbs component and it's working with styles  and it's Sub-Component like List and Item

--- theme/components/Breadcrumbs/index.tsx
+++ theme/components/Breadcrumbs/index.tsx
@@ -1,29 +1,38 @@
 'use client';
-import { Children, MouseEvent, MouseEventHandler, ButtonHTMLAttributes, HTMLAttributes, useMemo } from 'react'
+import { Children, MouseEvent } from 'react'
 import { twMerge as cn } from 'tailwind-merge'
 import * as DropdownPrimitive from '@radix-ui/react-dropdown-menu';
 import { HiChevronRight, HiEllipsisHorizontal } from 'react-icons/hi2';
-import { Slot } from '@radix-ui/react-slot';
+import { SizesType } from '../../../types';
 
 
-interface PropsType extends ButtonHTMLAttributes<HTMLButtonElement> { }
+interface PropsType {
+    children?: React.ReactNode;
+    size?: SizesType;
+    onClick?: (ItemValue: string, event: MouseEvent) => void
+}
 
 
 interface BreadcrumbsRootType extends React.FC<PropsType> {
     // List
-    List: React.FC<any>
+    List: React.FC<{ className?: string; children?: React.ReactNode }>
     // Item 
     Item: React.FC<ItemPropsType>
 }
 
-interface BreadcrumbType extends ItemPropsType { }
+interface BreadcrumbType extends Omit<ItemPropsType, 'value'> {
+    value: string;
+}
 
-const Breadcrumbs: BreadcrumbsRootType = ({ className, children, ...props }) => {
+const Breadcrumbs: BreadcrumbsRootType = ({ onClick, size = 'sm', children, ...props }) => {
 
-    const Breads: BreadcrumbType[] = useMemo(() => {
+    const { Breads, className }: { Breads: BreadcrumbType[], className: string } = (() => {
+
         const resArr: BreadcrumbType[] = [];
+
+        let className: string = ''
         // Looping in children to find the List subComponent
-        Children.toArray(children).forEach((child: any, idx) => {
+        Children.toArray(children).forEach((child: any) => {
 
             if (child.type?.displayName == 'BreadcrumbsList') {
                 // checking the children prop in the List component and looping in in to find the items and convert them to an array to show them 
@@ -41,30 +50,51 @@ const Breadcrumbs: BreadcrumbsRootType = ({ className, children, ...props }) => 
                         }
                     })
                 }
+
+                // save the styles of the List 
+                if (child.props.className) {
+                    className = child.props.className;
+                }
             }
 
         });
 
-        return resArr;
-    }, []);
+        return { Breads: resArr, className };
+    })();
 
 
+    // handle the clicks 
+
+    const HandleClick = (event: any, value: string, cb?: (e: MouseEvent) => void) => {
+        // calling the function of the item 
+        if (cb) cb(event);
+        // calling the function of the Root 
+        if (onClick) onClick(value, event);
+    }
 
     return (<div className={cn('w-max py-1 px-3 select-none bg-[#1966B21A] rounded-md flex gap-2 items-center', className)} {...props}>
 
         {
             Breads && Breads.length < 4 ?
                 Breads.map((b, idx, ar) =>
-                    <button onClick={b.onClick as never} key={b.value} className={cn('w-max px-1 inline-flex justify-center items-center gap-2 whitespace-nowrap rounded-sm text-base transition-colors cursor-pointer text-text-link hover:text-text-link-hover', 'outline-none select-none focus-visible:outline-1 focus-visible:outline-blue-500 focus-visible:text-text-link-hover ')}>
-                        {b.children} {idx < (ar.length - 1) ? <HiChevronRight className="w-5 h-5 text-icon-dark" /> : null}
-                    </button>
+                    <>
+                        <button
+                            onClick={(idx + 1 == ar.length) ? undefined : (e) => HandleClick(e, b.value, b.onClick)}
+                            key={b.value}
+                            className={cn('w-max px-1 inline-flex justify-center items-center gap-2 whitespace-nowrap rounded-sm text-base outline-none select-none', (idx + 1) == ar.length ? 'cursor-auto text-text-primary' : 'transition-colors cursor-pointer text-text-link hover:text-text-link-hover focus-visible:outline-1 focus-visible:outline-blue-500 focus-visible:text-text-link-hover')}
+                            children={b.children} />
+                        {(idx + 1) < (ar.length) ? <HiChevronRight className="w-5 h-5 text-icon-dark" /> : null}
+                    </>
                 )
                 : Breads ?
                     <>
                         {/* 1 */}
-                        <button onClick={Breads[0].onClick as never} key={Breads[0].value} className={cn('w-max px-1 inline-flex justify-center items-center gap-2 whitespace-nowrap rounded-sm text-base transition-colors cursor-pointer text-text-link hover:text-text-link-hover', 'outline-none select-none focus-visible:outline-1 focus-visible:outline-blue-500 focus-visible:text-text-link-hover ')}>
-                            {Breads[0].children}
-                        </button>
+                        <button
+                            onClick={e => HandleClick(e, Breads[0].value, Breads[0].onClick)}
+                            key={Breads[0].value}
+                            className={cn('w-max px-1 inline-flex justify-center items-center gap-2 whitespace-nowrap rounded-sm text-base transition-colors cursor-pointer text-text-link hover:text-text-link-hover', 'outline-none select-none focus-visible:outline-1 focus-visible:outline-blue-500 focus-visible:text-text-link-hover')}
+                            children={Breads[0].children}
+                        />
                         <HiChevronRight className="w-5 h-5 text-icon-dark" />
 
 
@@ -81,14 +111,15 @@ const Breadcrumbs: BreadcrumbsRootType = ({ className, children, ...props }) => 
                                 <DropdownPrimitive.Content
                                     side='bottom'
                                     sideOffset={4}
-                                    className={cn('group', 'relative w-[9em] mt-1 ml-14 max-h-[15.4em] sm-scrollbar overflow-y-auto rounded-md shadow-md bg-field-background flex flex-col p-1 gap-1')} data-size={'sm'}>
+                                    className="group relative w-[9em] mt-1 ml-14 max-h-[15.4em] sm-scrollbar overflow-y-auto rounded-md shadow-md bg-field-background flex flex-col p-1 gap-1"
+                                    data-size={size}>
                                     {/* remove the first item and item before the last and the last and looping through the array  */}
 
                                     {
                                         Breads.filter((_, i, a) => i !== 0 && i !== (a.length - 1) && i !== (a.length - 2)).map(e =>
                                             <DropdownPrimitive.Item
                                                 key={e.value}
-                                                onClick={e.onClick as never}
+                                                onClick={ev => HandleClick(ev, e.value, e.onClick)}
                                                 children={e.children}
                                                 className={cn(
                                                     'w-full flex justify-between cursor-pointer select-none items-center rounded-md px-4 text-base outline-none data-[disabled]:pointer-events-none',
@@ -110,16 +141,18 @@ const Breadcrumbs: BreadcrumbsRootType = ({ className, children, ...props }) => 
 
 
                         {/* the item before the last item  */}
-                        <button onClick={Breads.at(-2)?.onClick as never} key={Breads.at(-1)?.value} className={cn('w-max px-1 inline-flex justify-center items-center gap-2 whitespace-nowrap rounded-sm text-base transition-colors cursor-pointer text-text-link hover:text-text-link-hover', 'outline-none select-none focus-visible:outline-1 focus-visible:outline-blue-500 focus-visible:text-text-link-hover ')}>
-                            {Breads.at(-2)?.children}
-                        </button>
+                        <button
+                            onClick={e => HandleClick(e, Breads.at(-2)?.value as never, Breads.at(-2)?.onClick)}
+                            key={Breads.at(-2)?.value} className={cn('w-max px-1 inline-flex justify-center items-center gap-2 whitespace-nowrap rounded-sm text-base transition-colors cursor-pointer text-text-link hover:text-text-link-hover', 'outline-none select-none focus-visible:outline-1 focus-visible:outline-blue-500 focus-visible:text-text-link-hover ')}
+                            children={Breads.at(-2)?.children}
+                        />
                         <HiChevronRight className="w-5 h-5 text-icon-dark" />
 
                         {/* last item */}
-                        <button onClick={Breads.at(-1)?.onClick as never} key={Breads.at(-1)?.value} className={cn('w-max px-1 inline-flex justify-center items-center gap-2 whitespace-nowrap rounded-sm text-base transition-colors cursor-pointer text-text-link hover:text-text-link-hover', 'outline-none select-none focus-visible:outline-1 focus-visible:outline-blue-500 focus-visible:text-text-link-hover ')}>
-                            {Breads.at(-1)?.children}
-                        </button>
-
+                        <button
+                            key={Breads.at(-1)?.value}
+                            className={cn('w-max px-1 inline-flex justify-center items-center gap-2 cursor-auto whitespace-nowrap rounded-sm text-base text-text-primary', 'outline-none select-non')}
+                            children={Breads.at(-1)?.children} />
 
                     </>
                     : <></>
@@ -132,7 +165,7 @@ const Breadcrumbs: BreadcrumbsRootType = ({ className, children, ...props }) => 
 // List 
 
 Breadcrumbs.List = function () {
-    // this component is just for storing the Item  
+    // this component is just for storing the Items  
     return null;
 }
 Breadcrumbs.List.displayName = 'BreadcrumbsList';
@@ -142,8 +175,8 @@ Breadcrumbs.List.displayName = 'BreadcrumbsList';
 
 interface ItemPropsType {
     children: string;
-    value: string;
-    onClick?: (event: MouseEvent<HTMLDivElement, MouseEvent>) => MouseEventHandler<HTMLDivElement>
+    value?: string;
+    onClick?: (event: MouseEvent) => void
 }
 
 Breadcrumbs.Item = function () {
@@ -152,10 +185,6 @@ Breadcrumbs.Item = function () {
 }
 
 Breadcrumbs.Item.displayName = 'BreadcrumbsItem';
-
-
-
-
 
 
 export default Breadcrumbs;
