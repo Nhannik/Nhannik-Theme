30c68aecda7b00d8c7158967555523d571b90e6e
MESSELMII_YAHYA <107892703+MESSELMIyahya@users.noreply.github.com>
2024-02-19 22:34:36 +0100

    Finish Adding link variant to the button component

--- theme/components/Button/index.tsx
+++ theme/components/Button/index.tsx
@@ -1,18 +1,21 @@
 import { ButtonHTMLAttributes, forwardRef } from "react";
 import { twMerge } from "tailwind-merge";
-
+import { Slot } from "@radix-ui/react-slot";
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   iconOnly?: boolean;
+  asChild?:boolean;
   size?: "sm" | 'md' | "lg";
   className?: string;
-  variant?: "primary" | "tertiary" | "secondary" | "ghost" | "danger" | "danger-ghost" | "danger-tertiary"
+  variant?: "primary" | 'link' | "tertiary" | "secondary" | "ghost" | "danger" | "danger-ghost" | "danger-tertiary"
 }
 
-const Button = forwardRef<HTMLButtonElement, Props>(({ iconOnly, size, className, variant, ...props }, ref) => {
-  return <button ref={ref} className={twMerge('w-max inline-flex justify-center items-center gap-2 whitespace-nowrap rounded-md text-[14px] font-semibold transition-colors hover:transition-colors active:transition-colors outline-none select-none focus-visible:outline-blue-500'
+const Button = forwardRef<HTMLButtonElement, Props>(({ asChild=false,iconOnly, size, className, variant, ...props }, ref) => {
+  const Comp = asChild ? Slot : 'button' ;
+  return <Comp ref={ref}  className={twMerge('w-max inline-flex justify-center items-center gap-2 whitespace-nowrap rounded-md text-[14px] font-semibold transition-colors hover:transition-colors active:transition-colors outline-none select-none focus-visible:outline-blue-500'
     //  styling 
     , variant == 'primary' ? "text-text-oncolor bg-button-primary hover:bg-button-primary-hover active:bg-button-primary-selected disabled:bg-button-disabled disabled:text-text-disabled" :
+     variant == 'link' ? "text-link-default underline hover:text-link-hover visited:text-link-visited  disabled:text-text-disabled" :
       variant == 'secondary' ? "text-text-primary bg-button-secondary hover:bg-button-secondary-hover active:bg-button-secondary-selected border border-border-brand-dark disabled:border-border-disabled disabled:bg-button-disabled disabled:text-text-disabled" :
         variant == 'tertiary' ? "text-button-primary bg-button-tertiary hover:bg-button-tertiary-hover active:bg-button-tertiary-selected disabled:bg-button-disabled disabled:text-text-disabled border-solid border-[1px] disabled:border-transparent" :
           variant == 'ghost' ? "text-button-primary bg-button-tertiary hover:bg-button-tertiary-hover active:bg-button-tertiary-selected  disabled:text-neutral-400 disabled:bg-transparent" :
