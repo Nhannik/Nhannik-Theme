421cb3c6bbff5c94ab963ec7a236b5ebe7fe7cf6
WassimChouiha <chouihawassim@gmail.com>
2024-04-13 13:42:08 +0100

    added Tab component

---
+++ theme/components/Tabs/Tab.tsx
@@ -0,0 +1,136 @@
+'use client';
+import { useEffect, useMemo, useState } from "react";
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi2";
+import { twMerge } from "tailwind-merge";
+
+
+interface TabProps {
+    selected?: boolean;
+    disabled?: boolean;
+    style: '1' | '2';
+    onlyIcon?: boolean;
+    tertiary?: boolean;
+    bigIcon?: boolean;
+    vertical?: boolean;
+    border?: boolean;
+    className?: string;
+    value: string | number;
+    click: (n: string | number) => void;
+    
+    children: React.ReactNode | string
+}
+
+function Tab({ click, border, vertical, className, value, tertiary, onlyIcon, style, disabled, bigIcon, selected, children }: TabProps) {
+    return (<>
+
+        <div onClick={() => !disabled && !selected && click(value)} className={twMerge('w-max  px-4 flex transition-all justify-center items-center text-base select-none gap-2 '
+            , style == '2' ? "py-[14px]" : "py-[11px]",
+            onlyIcon ? style == '2' || bigIcon ? "p-[13px] text-xl" : "p-[11px]" : "",
+            className,
+            style == '2' ?
+                selected ? "   text-text-brand  border-border-selected" :
+                    "bg-button-tertiary-hover cursor-pointer text-text-secondary border-button-tertiary-hover hover:text-text-primary   transform-color  "
+                :
+                selected ?
+                    "   text-text-brand   border-border-selected" :
+                    "bg-transparent  cursor-pointer border-border-strong text-text-secondary hover:text-text-primary transform-color",
+            vertical ?
+                style == '2' ? "border-l-2" : "border-l-2"
+                : style == '2' ? "border-t-2" : "border-b-2",
+            border ? "border-r-[1px] border-r-border-strong" : ""
+            , selected ? tertiary ? "bg-transparent" : "bg-[#1966B21A]" : ""
+            ,
+            disabled ?
+                style == '2' ?
+                    "bg-button-disabled   text-text-disabled hover:text-text-disabled cursor-not-allowed border-border-disabled" :
+                    "bg-transparent  text-text-disabled hover:text-text-disabled cursor-not-allowed border-border-disabled"
+                : ""
+        )}>{children}</div>
+
+    </>)
+}
+
+
+interface List {
+    value: string | number;
+    content: React.ReactNode;
+    bigIcon?: boolean;
+    onlyIcon?: boolean;
+    disabled?: boolean;
+}
+
+interface Props {
+    tabsList?: List[];
+    style?: "line" | "contained";
+    value: string | number;
+    tertiary?: boolean;
+    vertical?: boolean;
+    disabled?: boolean;
+    overflow?: number;
+    className?: string;
+    onChange: (n: string | number) => void;
+}
+
+export default function Tabs({ value, vertical, className, disabled, tertiary, overflow, onChange, style = 'line', tabsList = [] }: Props) {
+    const [po, setPo] = useState<number>(0);
+    const tabsList2: List[] = useMemo(() => overflow ? tabsList.slice(po, (po) + overflow) : [], [po]) ;
+
+
+    function add() {
+        overflow && setPo(e => e + overflow < tabsList.length ? e + 1 : e);
+    }
+    function remove() {
+        overflow && setPo(e => e > 0 ? e - 1 : e);
+    }
+
+    return (<div className={twMerge("flex ", vertical ? "flex-col" : "items-center")}>
+
+
+        {
+            overflow && !vertical ? <>
+
+
+                <button disabled={po != 0?disabled:true} onClick={remove} className={twMerge('flex mr-2 justify-center items-center text-md focus-visible:outline-button-primary',
+                style == 'contained' ?
+                "w-[48px] h-[48px] text-icon-dark bg-button-tertiary-hover disabled:bg-button-disabled disabled:text-icon-disabled"
+                : "w-[44px]  h-[44px] text-icon-dark disabled:text-icon-disabled"
+                
+                ,)}>
+                    <HiChevronLeft />
+                </button>
+
+
+                {
+
+                    tabsList2.map((e, i, a) =>
+                        <Tab key={i+''+e.value} bigIcon={e.bigIcon} className={className} vertical={vertical} tertiary={vertical ? true : tertiary} onlyIcon={e.onlyIcon} selected={value == e.value} disabled={e.disabled || disabled} border={value != e.value && value != a[i+1]?.value && i != (a.length - 1) && style == 'contained'} value={e.value} children={e.content} click={(e) => onChange(e)} style={style == 'contained' ? '2' : '1'} />
+                    )
+                }
+
+
+                <button disabled={po + overflow != tabsList.length?disabled:true} onClick={add} className={twMerge('flex ml-2 justify-center items-center text-md focus-visible:outline-button-primary',
+                style == 'contained' ?
+                "w-[48px] h-[48px] text-icon-dark bg-button-tertiary-hover disabled:text-icon-disabled"
+                : "w-[44px]  h-[44px] text-icon-dark disabled:text-icon-disabled"
+                
+                ,)}>
+                    <HiChevronRight />
+                </button>
+                
+            
+
+            </> : <>
+                {
+                    tabsList.map((e, i, a) =>
+                        <Tab key={i+''+e.value} bigIcon={e.bigIcon} vertical={vertical} tertiary={vertical ? true : tertiary} onlyIcon={e.onlyIcon} className={className} selected={value == e.value} disabled={e.disabled || disabled} border={value != e.value && i != (a.length - 1) && style == 'contained'} value={e.value} children={e.content} click={(e) => onChange(e)} style={style == 'contained' ? '2' : '1'} />
+                    )
+
+                }
+            </>
+        }
+
+
+
+    </div>);
+
+}
