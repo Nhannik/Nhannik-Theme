d1f8bee3f2fb5175ddcf4f88a3f62c3b881b04cd
anaskhader <khaderanastec@gmail.com>
2024-04-21 14:54:02 +0100

    minor changes

--- theme/components/Tooltip/STooltip.tsx
+++ theme/components/Tooltip/STooltip.tsx
@@ -1,30 +1,30 @@
 import { twMerge } from "tailwind-merge";
-import {PlacesType,Tooltip as Tp} from "react-tooltip";
+import { PlacesType, Tooltip as Tp } from "react-tooltip";
 
 interface Props {
-    children:React.ReactNode;
-    content:string|React.ReactNode;
-    size?:'sm'|'md'|"lg"
-    id:string;
-    inline?:boolean;
-    className?:string;
+    children: React.ReactNode;
+    content: string | React.ReactNode;
+    size?: 'sm' | 'md' | "lg"
+    id: string;
+    inline?: boolean;
+    className?: string;
     position?: PlacesType;
 }
 
-export default function SToolTip({inline,className,id,children,content,position,size}:Props){
-    return   <>
-    <div className={twMerge(inline?'inline':'')} data-tooltip-id={id}>{children}</div>
-     <Tp 
-     id={id} place={position} opacity={1}  
-     className={className}
-     style={{
-     maxWidth:'18em',
-     backgroundColor:'#262626',
-     padding:size == 'md' ? '8px 16px' : size == 'lg' ? "16px" : "2px 16px" ,
-     fontSize:14,
-     borderRadius:2,
-     zIndex:100,
-     }} 
-      >{content}</Tp>
+export default function SToolTip({ inline, className, id, children, content, position, size }: Props) {
+    return <>
+        <div className={twMerge(inline ? 'inline' : '')} data-tooltip-id={id}>{children}</div>
+        <Tp
+            id={id} place={position} opacity={1}
+            className={className}
+            style={{
+                maxWidth: '18em',
+                backgroundColor: '#262626',
+                padding: size == 'md' ? '8px 16px' : size == 'lg' ? "16px" : "2px 16px",
+                fontSize: 14,
+                borderRadius: 2,
+                zIndex: 100,
+            }}
+        >{content}</Tp>
     </>
 }
